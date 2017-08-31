8ca0734f83ff5171f4f908323156a312174a0470
kty_raspberrypi <kty@naver.com>
2017-08-31 12:41:21 +0900

    이것은 LEDLogTest입니다.

---
+++ LEDLogTest.py
@@ -0,0 +1,42 @@
+#coding: utf-8
+import RPi.GPIO as GPIO
+import time
+import datetime as d
+import os
+
+#constant
+LED = 8
+#GPIO Set
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED, GPIO.OUT, initial = GPIO.LOW)
+
+def turnOn():
+        GPIO.output(LED, GPIO.HIGH)
+        addLog("LED가 커졌습니다.")
+def turnOff():
+        GPIO.output(LED, GPIO.LOW)
+        addLog("LED가 꺼졌습니다.")
+def addLog(msg):
+	os.system("echo " + msg + "\(" + getCurrentTime() +"\) >> ledlog.txt")
+        print msg
+
+def getCurrentTime():
+	now = d.datetime.now()
+	return now.strftime("%Y-%m-%d:%H:%M:%S")
+
+
+addLog("프로그램을 실행합니다.")
+
+while True:
+        try:
+                turnOn()
+                time.sleep(3)
+
+                turnOff()
+                time.sleep(3)
+
+        except KeyboardInterrupt:
+		GPIO.cleanup()
+		addLog("프로그램을 종료합니다.")
+		break
+
