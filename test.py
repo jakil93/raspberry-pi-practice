36470aaf0d454c8deb10b61a8960c82eebc44ebf
kty_raspberrypi <kty@naver.com>
2017-08-31 12:32:14 +0900

    hello

---
+++ test.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+import RPi.GPIO as GPIO
+from dht11.dht11 import DHT11
+import time
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+
+instance = DHT11(pin = 8)
+result = instance.read()
+
+while True:
+	try:
+		msg = ""
+		time.sleep(2)
+		if result.is_valid():
+			msg += "온도 : " + str( result.temperature) + "C\n"
+			msg += "습도 : " + str( result.humidity) + "\n"
+		else:
+			msg += "Error : " + str( result.error_code)
+		print msg, "현재시각 : ", time.time()
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+		break
