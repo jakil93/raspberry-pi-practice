48bb1a03f4ba18e3224fe2bbd1d1fc96fdcb102b
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-01 10:21:43 +0900

    send big Mail 첫버전

--- sendbigmail.py
+++ sendbigmail.py
@@ -1,6 +1,16 @@
 #coding: utf-8
 
-import time
-import flask
+import flask import Flask
 
-print "test"
+#make instance
+app = Flask(__name__)
+
+#main page
+@app.route("/")
+def main():
+	return "여기는 태영이 홈페이지다"
+
+#start server!
+if __name__ = "__main__":
+	app.run(debug = True, host = "0.0.0.0", port = 8888)
+
