c759a168891c0377d29c0d744d517abcdea9e529
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-04 09:53:19 +0900

    no message

--- flask_led.py
+++ flask_led.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 import RPi.GPIO as GPIO
 import time
+import logger
 
 from flask import Flask, request, render_template
 
@@ -12,7 +13,7 @@ GPIO.setwarnings(False)
 #set GPIO Pins
 LED_RED = 14
 LED_YELLOW = 15
-
+LOGGER_FILE_NAME = "flask_led_log"
 
 #set GPIO direction (IN / OUT)
 GPIO.setup(LED_RED, GPIO.OUT, initial=GPIO.LOW)
@@ -25,6 +26,8 @@ app = Flask(__name__)
 def ledon():
 
 	target = str( request.args.get("target") )
+	logger.log(LOGGER_FILE_NAME, "led on.. target : " + target)
+
 	if target == "red":
 		GPIO.output(LED_RED, GPIO.HIGH)
 		result = "ledred turn on"
@@ -40,6 +43,7 @@ def ledon():
 def ledoff():
 
 	target = str( request.args.get("target") )
+	logger.log(LOGGER_FILE_NAME, "led off.. target : " + target)
 	if target == "red":
 		GPIO.output(LED_RED, GPIO.LOW)
 		result = "ledred turn off"
@@ -53,12 +57,14 @@ def ledoff():
 
 @app.route("/ledalloff", methods=["GET"])
 def ledalloff():
+	logger.log(LOGGER_FILE_NAME, "led all off")
 	GPIO.output(LED_RED, GPIO.LOW)
 	GPIO.output(LED_YELLOW, GPIO.LOW)
 	return "all led turn off"
 
 @app.route("/ledallon", methods=["GET"])
 def ledallon():
+	logger.log(LOGGER_FILE_NAME, "led all on")
 	GPIO.output(LED_RED, GPIO.HIGH)
 	GPIO.output(LED_YELLOW, GPIO.HIGH)
 	return "all led turn on"
