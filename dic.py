7a1f62fe1ea709d26b5c140205f5930e7e50664b
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-04 12:11:30 +0900

    no message

--- dic.py
+++ dic.py
@@ -4,21 +4,21 @@ def articles():
 		{'id' : 1,
 		 'title' : 'Article One',
 		 'content' : 'this is article one!',
-		 'author' : 'KTY'
+		 'author' : 'KTY',
 		 'create_date' : '04-09-2017'
 		}, 
 
 		{'id' : 2,
 		 'title' : 'Article Two',
 		 'content' : 'this is article two!',
-		 'author' : '강태영'
+		 'author' : '강태영',
 		 'create_date' : '04-09-2017'
 		}, 
 
 		{'id' : 3,
 		 'title' : 'Article Three',
 		 'content' : 'this is article three!',
-		 'author' : '태영이'
+		 'author' : '태영이',
 		 'create_date' : '04-09-2017'
 		}
 	];
