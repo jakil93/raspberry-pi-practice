07f7e8651f0941ec86019436b7f24ed589cf9da3
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-04 12:08:38 +0900

    no message

---
+++ dic.py
@@ -0,0 +1,25 @@
+#coding: utf-8
+def articles():
+	articles = [
+		{'id' : 1,
+		 'title' : 'Article One',
+		 'content' : 'this is article one!',
+		 'author' : 'KTY'
+		 'create_date' : '04-09-2017'
+		}, 
+
+		{'id' : 2,
+		 'title' : 'Article Two',
+		 'content' : 'this is article two!',
+		 'author' : '강태영'
+		 'create_date' : '04-09-2017'
+		}, 
+
+		{'id' : 3,
+		 'title' : 'Article Three',
+		 'content' : 'this is article three!',
+		 'author' : '태영이'
+		 'create_date' : '04-09-2017'
+		}
+	];
+	return articles
---
+++ flask_9.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import dic
+
+app = Flask(__name__)
+
+articles = dic.articles()
+
+@app.route('/')
+def index():
+	return 'hi dictionary'
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html', articles = articles)
+
+@app.route('/article/<int:id>')
+def article(id)
+	return render_template('article.html', id = id, articles= articles)
+if __name__ == '__main__'
+	app.run(debug=True, host="0.0.0.0", port=8888)
