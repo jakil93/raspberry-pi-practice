89b6126521b2fa132b05dbde6395b024af00f08e
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-04 10:46:54 +0900

    no message

---
+++ flask_6.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/posttest", methods=["POST"])
+def posttest():
+	name = request.form['name']
+	return render_template("flask_6.html", name=name)
+
+@app.route("/")
+def main():
+	return render_template("main2.html")
+
+if __name__ == "__main__":
+	app.run(debug = True, host = "0.0.0.0", port = 8888)
+	print "서버를 종료합니다."
