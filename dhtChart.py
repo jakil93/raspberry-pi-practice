90105c89cf6d1a60849bc43c2712a75cccd8a975
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-05 12:15:05 +0900

    no message

--- dhtChart.py
+++ dhtChart.py
@@ -1,34 +1,35 @@
 #coding: utf-8
-import sys
+import sys, time
 import Adafruit_DHT
+import httplib, urllib
+
+#API Key
+KEY = "W3T1G60VYTY05ZCZ"
 
 
-# Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-    print "PIN번호 : " + pin
-else:
-    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
-    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
-    sys.exit(1)
+#set GPIO.BCM
+humidity, temperature = Adafruit_DHT.read_retry(11, 3)
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+def sendData():
+    if humidity is not None and temperature is not None:
+        print('온도 : {0:0.1f}*  습도 : {1:0.1f}% 정보를 보냈습니다..'.format(temperature, humidity))
+        params = urllib.urlencode({'field1' : temperature,
+                                    'field2' : humidity,
+                                    'key' : KEY})
+        con = httplib.HTTPSConnection()
 
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
+        try:
+            con.request("POST", "https://api.thingspeak.com/update")
+            con.send(params)
+            resp = con.getresponse()
+            print resp.status, resp.reason
+        except:
+            print "Error! Connection Fail!"
+    else:
+        print('정보를 불러오는데 실패했습니다..')
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+while 1:
+    sendData()
+    time.sleep(5)
+
+print "프로그램을 종료합니다."
