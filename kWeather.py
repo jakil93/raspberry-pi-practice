26a94138db9b455add8da0fe321fea2b94331b2e
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-05 14:20:50 +0900

    no message

--- kWeather.py
+++ kWeather.py
@@ -4,8 +4,7 @@ import time, httplib, urllib, json
 #set program info
 KEY = "90c4f0c8-d7b4-38a2-a221-701ad624f172"
 URL = "/weather/summary?version=1&lat=37.5714000000&lon=126.9658000000&stnid=108&appKey=" + KEY
-header = {"Content-Type":"application/x-www-form-urlencoded",
-            "Accept" : "application/json"}
+header = {"Accept" : "application/json"}
 
 con = httplib.HTTPConnection("apis.skplanetx.com")
 
