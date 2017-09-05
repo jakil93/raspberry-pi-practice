549639808b1684bbba2906dc5ac292c93def69d4
DESKTOP-KKAQ0RK\it <jakil93@naver.com>
2017-09-05 10:46:04 +0900

    no message

--- ultrasonic_thing.py
+++ ultrasonic_thing.py
@@ -64,5 +64,5 @@ while 1:
         print resp.status, resp.reason
     except:
         print "Error! Connection Fail!"
-    print "측정된 거리 : " + dist
+    print "측정된 거리 : " + str(dist)
     time.sleep(5)
